6f47916ebea29cf39848e0ece398c55b341b0905
Kattni Rembor <kattni@adafruit.com>
2018-08-06 15:36:30 -0400

    Returned example to original state, pylintrc updated

--- examples/showbcddigits.py
+++ examples/showbcddigits.py
@@ -1,13 +1,13 @@
 import time
 import random
-from board import TX, RX, D1
+from board import TX, RX, A1
 import busio
 import digitalio
 from adafruit_max7219 import bcddigits
 
 mosi = TX
 clk = RX
-cs = digitalio.DigitalInOut(D1)
+cs = digitalio.DigitalInOut(A1)
 
 spi = busio.SPI(clk, MOSI=mosi)
 
