3fabbb83818937f16e7cf0b431f6067593b80f20
OldCodeFans <songjiangzhang@hotmail.com>
2021-04-06 23:10:02 +0800

    Update max7219_cascade.py

--- examples/max7219_cascade.py
+++ examples/max7219_cascade.py
@@ -13,18 +13,27 @@ spi = busio.SPI(clk, MOSI=din)
 display = matrices.Matrix8x8(spi, cs)
 
 while True:
-    time.sleep(3.0)
-    display.clear_all()
-    s = 'Hello, World!'
-    display.text(s[random.randint(0,len(s)-1)],0,0)
-
-    # show random char in string s on the the first of cascaded matrixs.
-    display.show(1,2)
-
-    for c in range(len(s)*8):
-        display.fill(0)
-        display.text(s,-c,0)
+        display.clear_all()
+        s = 'Hello, World!'
         
-        # show scrolled string on the second of cascaded matrixs.
-        display.show(2,2) 
-        time.sleep(0.25)
+        #Demo One: cascaded matrix with two max7219
+        # show random char in string s on the the first of cascaded matrixs.
+        # show scroll string s on the second of cascaded matrixs.
+        display.text(s[random.randint(0,len(s)-1)],0,0)
+        display.show(1,2)
+        
+        for c in range(len(s)*8):
+                display.fill(0)
+                display.text(s,-c,0)
+                display.show(2,2) 
+                time.sleep(0.25)
+                
+        #Demo two: scroll string s on the cascaded matrix with two max7219
+        for c in range(len(s)*8):
+                display.fill(0)
+                display.text(s,-c,0)
+                display.show(1,2)
+                display.fill(0)
+                display.text(s,-c+8,0)
+                display.show(2,2)
+                time.sleep(0.25)
